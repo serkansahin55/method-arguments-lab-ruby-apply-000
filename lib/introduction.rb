7798e291f7a28b3895ad4e29829a6991a5abdd43
Serkan Sahin <ssahin55@outlook.com>
2019-11-19 18:11:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,14 @@
-# lib/introduction.rb 
-
 def introduction(name)
-  puts "Hi, my name is #{name}."
+  message = "Hi, my name is #{name}."
+  puts message
 end
 
-def introduction_with_language(name , language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+
+def introduction_with_language(name, language)
+  message = "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts message
 end
+  
+  
+  
+#Hi, my name is Dan and I am learning to program in Ember.j
